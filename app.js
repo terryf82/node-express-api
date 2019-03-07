6079f9ebfb39d21373ef45c7ec1895af0fcfd147
Terry Franklin <terryfranklin82@gmail.com>
2019-03-08 09:50:38 +1000

    more book details, search by genre

--- app.js
+++ app.js
@@ -4,15 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-if (process.env.ENV === 'Test') {
-    console.log('This is a test');
-    const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
-}
-else {
-    console.log('This is for real');
-    const db = mongoose.connect('mongodb://localhost/bookAPI');
-}
-
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
 const bookRouter = require('./routes/bookRouter')(Book);
